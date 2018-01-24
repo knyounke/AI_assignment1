98481083a774a31738f662dc31cefdd0ea4a2795
Katie Younke <knyounke@gmail.com>
2018-01-23 19:36:16 -0600

    first commit

---
+++ find_route.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+
+
+public class find_route{
+	String label;
+	ArrayList<Node> adjacencyList;
+
+	public static void main(String[] args){
+		HashMap<String,Node> graph = new HashMap<String,Node>();
+	}
+
+}
