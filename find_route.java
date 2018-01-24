1ac8de97b6dfc27433decfa3f0b2536cdd2bc01e
Katie Younke <knyounke@gmail.com>
2018-01-23 20:25:38 -0600

    Formulating plan of action

--- find_route.java
+++ find_route.java
@@ -2,13 +2,35 @@ import java.io.*;
 import java.util.*;
 
 
-
 public class find_route{
-	String label;
-	ArrayList<Node> adjacencyList;
+	
+	//public node pop(queue)
+
 
 	public static void main(String[] args){
-		HashMap<String,Node> graph = new HashMap<String,Node>();
+	//READ THE FILE INTO A WEIGHTED ADJACENCY LIST
+		PriorityQueue<Node> frontier;
+		String start, goal;
+		
+
+	//PERFORM PROBLEM SOLVING AGENT USING UNIFORM COST SEARCH & GRAPH SEARCH
+		/*node; <--a node with a state
+		frontier; <--queue
+		explored; 
+
+		do(){
+		if(frontier == NULL){ return 0;}
+		node = pop(frontier);
+		}*/
+
+	
 	}
 
 }
+
+//step 1: read open the file
+//step 2: separate values based on spaces and organize each line as a state//fe
+//step 3: close the file
+//step 4: initialize arg[1] as initial state in the frontier and arg[2] as goal state and explored set as empty
+//step 5: loop throug
+
