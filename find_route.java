6385e0d257ebdac9132c8dc234e6cb82a9468989
Katie Younke <knyounke@gmail.com>
2018-01-28 23:46:15 -0600

    further comments on action

--- find_route.java
+++ find_route.java
@@ -9,12 +9,14 @@ public class find_route{
 
 	public static void main(String[] args){
 	//READ THE FILE INTO A WEIGHTED ADJACENCY LIST
-		PriorityQueue<Node> frontier;
+		ArrayList<String> closed_set = new ArrayList<String>();
+		PriorityQueue<Node> fringe;
 		String start, goal;
 		
 
 	//PERFORM PROBLEM SOLVING AGENT USING UNIFORM COST SEARCH & GRAPH SEARCH
-		/*node; <--a node with a state
+		/*
+		node; <--a node with a state
 		frontier; <--queue
 		explored; 
 
@@ -29,8 +31,8 @@ public class find_route{
 }
 
 //step 1: read open the file
-//step 2: separate values based on spaces and organize each line as a state//fe
+//step 2: separate values based on spaces and organize each city name as a new state
 //step 3: close the file
 //step 4: initialize arg[1] as initial state in the frontier and arg[2] as goal state and explored set as empty
-//step 5: loop throug
+//step 5: loop through
 
